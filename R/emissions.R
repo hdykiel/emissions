375b27d4b652ca814aa557a496822de6442b2c70
OpsWorks user hdykiel <hdykiel@rsp1.localdomain>
2019-12-16 22:23:09 +0000

    fix typo

--- R/emissions.R
+++ R/emissions.R
@@ -13,13 +13,13 @@
 #' emissions("plane", 1000)
 emissions <- function(transport_mode = "plane", method = "distance", value = 1) {
 
-  if (transport_mode == "plane" && method = "distance" ) {
+  if (transport_mode == "plane" && method == "distance" ) {
     CO2_grams <- 175*value # C02 grams
     CO2_kg <- CO2_grams/1000
     CO2_ton <- CO2_kg/1000
     return(CO2_ton)
 
-  } else if (transport_mode == "plane" && method = "spend") {
+  } else if (transport_mode == "plane" && method == "spend") {
     CO2_kg <- 0.15*value
     CO2_ton <- CO2_kg/1000
     return(CO2_ton)
