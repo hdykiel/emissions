bccc2373e01e88defd7f8577e7d4c66f6e0f191e
OpsWorks user hdykiel <hdykiel@rsp1.localdomain>
2019-12-16 22:19:19 +0000

    add spend-based calculation method

--- R/emissions.R
+++ R/emissions.R
@@ -3,24 +3,28 @@
 #' Estimate carbon dioxide emissions from travel
 #'
 #' @param transport_mode
-#' @param kilometers
+#' @param method
+#' @param value
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' emissions("plane", 1000)
-emissions <- function(transport_mode = "plane", kilometers = 1) {
+emissions <- function(transport_mode = "plane", method = "distance", value = 1) {
 
-  if (transport_mode == "plane") {
-    CO2_grams <- 175*kilometers # C02 grams
+  if (transport_mode == "plane" && method = "distance" ) {
+    CO2_grams <- 175*value # C02 grams
     CO2_kg <- CO2_grams/1000
     CO2_ton <- CO2_kg/1000
     return(CO2_ton)
 
+  } else if (transport_mode == "plane" && method = "spend") {
+    CO2_kg <- 0.15*value
+    CO2_ton <- CO2_kg/1000
+    return(CO2_ton)
+
   } else {
-
     stop("please enter a valid transportation mode")
-
   }
 }
