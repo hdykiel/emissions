513e55ed0eb1fe3df673f6eebe80376447dbca84
skeydan <keydana@gmx.de>
2020-10-07 21:00:42 +0200

    remove spend

--- R/emissions.R
+++ R/emissions.R
@@ -1,13 +1,11 @@
 #' Carbon Emissions Calculator
 #'
-#' Estimate green house gas emissions from travel
+#' Estimate green house gas emissions (CO2e)
 #'
 #' @param category category to calculate emissions for. Currently supported are:
 #'  "airplane" for air travel.
-#' @param type category-dependent additional information. For air travel: "distance" or "spend".
-#' If you choose "spend", a default conversion factor will be used that maps spend to distance.
-#' @param value category-dependent value. For air travel, integer vector of either kilometers
-#' traveled or spend in USD. E.g., `value = c(220,800,2500)`.
+#' @param value category-dependent value. For air travel, integer vector of kilometers
+#' traveled. E.g., `value = c(220,800,2500)`.
 #'
 #' @return CO2 equivalent emissions in tons
 #' @import dplyr
@@ -15,21 +13,21 @@
 #' @export
 #'
 #' @examples
-#' emissions("airplane", "distance", c(100, 1000, 10000))
+#' emissions("airplane", c(100, 1000, 10000))
 #'
-emissions <- function(category, type, value) {
+emissions <- function(category, value) {
 
   available_categories <- c("airplane")
   if (!(category %in% available_categories)) stop("Please provide a valid category")
 
   co2e_ton <- switch(category,
-         airplane = co2e_airplane(type, value)
+         airplane = co2e_airplane(value)
   )
   co2e_ton
 }
 
 #' @import dplyr
-co2e_airplane <- function(method, value) {
+co2e_airplane <- function(value) {
 
   equivalents <- suppressMessages(readr::read_csv(system.file("conf", "global_warming_potential.conf", package = "emissions")))
   ch4_eq <- equivalents %>% dplyr::filter(ghg == "ch4") %>% dplyr::pull()
@@ -52,19 +50,9 @@ co2e_airplane <- function(method, value) {
     dplyr::filter(haul == "long") %>%
     dplyr::select(co2_kg, ch4_g, n2o_g)
 
-  # aller-retour?
-  # maybe have to use an "average" haul ...
-  if (method == "spend") {
-    kms <- Map(price_to_km_air_travel, value) %>% unlist()
-  } else if (method == "distance") {
-    kms <- value
-  } else {
-    stop("Valid methods for air travel are distance or spend")
-  }
-
-  short_flight_kms <- Filter(function(x) x < threshold_medium, kms) %>% sum()
-  medium_flight_kms <- Filter(function(x) between(x, threshold_medium, threshold_long), kms) %>% sum()
-  long_flight_kms <- Filter(function(x) x > threshold_long, kms) %>% sum()
+  short_flight_kms <- Filter(function(x) x < threshold_medium, value) %>% sum()
+  medium_flight_kms <- Filter(function(x) between(x, threshold_medium, threshold_long), value) %>% sum()
+  long_flight_kms <- Filter(function(x) x > threshold_long, value) %>% sum()
 
   short_flight_co2eq <-
     short_flight_kms * (
@@ -94,6 +82,3 @@ co2e_airplane <- function(method, value) {
 }
 
 miles_to_km <- function(miles) miles * 1.609344
-
-price_to_km_air_travel <- function(price) (price - 50)/0.11
-
